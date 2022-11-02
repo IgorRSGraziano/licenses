9e027995a539ac40a8c842e0479b5b1564ecd3b3
igorrsgraziano <igor.raphael@openk.com.br>
2022-11-02 13:08:47 -0300

    add licenses crud

---
+++ app/views/licenses/_license.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! license, :id, :key, :status, :created_at, :updated_at
+json.url license_url(license, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: "licenses#index"
+
+  resources :licenses
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
