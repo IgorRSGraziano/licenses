e4a499f8c76af7c0f2fd37dadc515aa87d116a92
igorrsgraziano <igor.raphael@openk.com.br>
2022-11-02 17:09:27 -0300

    styled license screen

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'securerandom'
+
+status = %w[active inactive suspended]
+
+(1..20).each { |_|
+  License.create(
+    key: SecureRandom.uuid,
+    status: status.sample
+  )
+}
