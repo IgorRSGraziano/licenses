574bac6764dbe34c87934eeadf198ae78480e739
igorrsgraziano <igorrsgraziano@gmail.com>
2022-11-15 20:07:18 -0300

    added sort arrow

--- app/helpers/licenses_helper.rb
+++ app/helpers/licenses_helper.rb
@@ -1,11 +1,17 @@
 module LicensesHelper
-  def filter_arrow
-    if params[:sort].present?
-      if params[:sort] == 'asc'
-        return 'fa fa-arrow-down'
+  def sort_arrow(url, param)
+    uri = URI.parse(url)
+    query = Rack::Utils.parse_query(uri.query)
+    sort = query['sort']
+    sort_order = sort.split(' ')[1]&.downcase
+    sort_field = sort.split(' ')[0]&.downcase
+    if (!sort_order.nil? || !sort_field.nil?) && sort_field == param.downcase
+      if sort_order == 'asc'
+        return 'arrow-down'
       else
-        return 'fa fa-arrow-up'
+        return 'arrow-up'
       end
     end
   end
+
 end
