d9b19d31ce594b7da7143860df687c10dfef729b
igorrsgraziano <igorrsgraziano@gmail.com>
2022-11-14 21:09:47 -0300

    fix layout from license

--- app/models/license.rb
+++ app/models/license.rb
@@ -1,5 +1,6 @@
 class License < ApplicationRecord
   enum status: { active: 0, inactive: 1, suspended: 2 }
-  belongs_to :customer
-  belongs_to :payment
+  belongs_to :customer, required: false
+  belongs_to :payment, required: false
+
 end
