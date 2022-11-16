0e54de2cf19e130f61a99630349d3f2b66f60672
igorrsgraziano <igorrsgraziano@gmail.com>
2022-11-15 23:41:07 -0300

    refact email license

--- app/mailers/license_mailer.rb
+++ app/mailers/license_mailer.rb
@@ -3,7 +3,7 @@ class LicenseMailer < ApplicationMailer
     @brand = ENV['BRAND_NAME']
     @license = args[:license]
     mail(to: Rails.env.production? ? args[:to] : ENV['SMTP_TEST_TO'],
-         subject: "Seu Token de ativação #{@brand}")
+         subject: "CHAVE #{@brand}")
   end
 
   def cancel_license(args)
