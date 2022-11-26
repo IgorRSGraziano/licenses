fb4bda9bafddf66e5385722d2c731c3db101681c
igorrsgraziano <igorrsgraziano@gmail.com>
2022-11-26 20:04:51 -0300

    add tomselect

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require tom-select
 import "@hotwired/turbo-rails"
 import "controllers"
+
+console.log("Hello")
