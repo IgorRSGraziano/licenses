96fd9a87b14e87ab30599ff20e43b374045aed5c
IgorRSGraziano <igorrsgraziano@gmail.com>
2022-12-11 21:32:28 +0000

    add updates in controller from user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -20,13 +20,20 @@ class UserController < ApplicationController
   end
 
   def update
-    @user.update(user_params)
+    @user.update user_params
+    @user.client.update brand: params[:brand]
+
+    Parameter.all.each do |parameter|
+      param_value = params["param_#{parameter.id}"]
+      parameter = ParametersClient.create_or_find_by client_id: @user.client.id, parameter_id: parameter.id
+      parameter.update value: param_value
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :username)
+    params.permit(:email, :password, :username)
   end
 
   def set_user
