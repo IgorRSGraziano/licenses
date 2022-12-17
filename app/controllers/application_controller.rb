03af7e35e48f6b1fe126c71ce9a07761d267e3b1
IgorRSGraziano <igorrsgraziano@gmail.com>
2022-12-17 01:31:05 +0000

    add validations from login and session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in?
 
   def current_user
-    User.find session[:user_id]
+    User.joins(:client).where(clients: { active: true }).find_by_id(session[:user_id])
   end
 
   def logged_in?
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :authorized, only: %i[new create]
 
   def create
-    user = User.find_by(email: params[:email])
+    user = User.joins(:client).where(clients: { active: true }).find_by(email: params[:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: 'Logged in!'
