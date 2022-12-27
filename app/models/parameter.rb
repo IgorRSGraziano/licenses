30a5ee7325d42f524ebda1c57d0f0da881f0a53a
IgorRSGraziano <igorrsgraziano@gmail.com>
2022-12-27 18:59:59 +0000

    fix nullable problem

--- app/models/parameter.rb
+++ app/models/parameter.rb
@@ -2,6 +2,6 @@ class Parameter < ApplicationRecord
   has_many :parameters_clients
 
   def value(client_id)
-    ParametersClient.find_by(client_id:, parameter_id: id).value
+    ParametersClient.find_by(client_id:, parameter_id: id)&.value
   end
 end
