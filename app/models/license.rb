7f68a3a1e26cb3fa1c63344a548f1da8c1c44bc9
Igor Graziano <igorrsgraziano@gmail.com>
2022-12-28 22:18:39 +0000

    fixed client name

--- app/models/license.rb
+++ app/models/license.rb
@@ -2,5 +2,6 @@ class License < ApplicationRecord
   enum status: { active: 0, inactive: 1, suspended: 2 }
   belongs_to :customer, required: false
   belongs_to :payment, required: false
+  belongs_to :client, required: true
 
 end
