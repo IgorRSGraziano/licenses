13001884bc1ec047e5b204eb910ff3920a88a060
Igor Graziano <igorrsgraziano@gmail.com>
2022-12-29 16:32:13 +0000

    add license screen in pt-BR

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,14 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def self.h(attr = nil)
+    return pl(1) if attr.nil?
+    return I18n.t "activerecord.#{attr}" if %i[updated_at created_at].include?(attr)
+
+    human_attribute_name attr
+  end
+
+  def self.pl(count = 2)
+    model_name.human count:
+  end
 end
